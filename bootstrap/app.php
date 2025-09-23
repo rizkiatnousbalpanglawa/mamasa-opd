356d9c4c61d3658e7ffb9ab575dcac8f6b9d7f4f
rizkiatnousbalpanglawa <usbalsaleko@gmail.com>
2025-09-24 04:13:27 +0800

    First Commit

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->append(TrustProxies::class);
+        // $middleware->append(TrustProxies::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
